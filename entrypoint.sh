df6709307dfff10bc9518f07dd8a11e12a2c67c8
frankii91 <14099566+frankii91@users.noreply.github.com>
2025-04-11 02:42:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ TMP_DIR=/tmp/code
 rm -rf "$TMP_DIR"
 
 # klonuj repo do TMP_DIR
-git clone --depth=1 https://github.com/user/repo.git "$TMP_DIR"
+git clone --depth=1 https://github.com/frankii91/schema-extractor.git "$TMP_DIR"
 
 # skopiuj zawartość do /app (nadpisz pliki, zachowaj inne)
 cp -r "$TMP_DIR"/* /app/
